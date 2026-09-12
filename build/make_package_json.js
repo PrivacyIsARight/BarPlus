28fe92176fca85b11f829e72798ef979dcfc7c2e
PrivacyIsARight <vexalous@gmail.com>
2026-09-12 09:12:42 -0400

    Change release source to this repository
    Signed-off-by: PrivacyIsARight <vexalous@gmail.com>

--- build/make_package_json.js
+++ build/make_package_json.js
@@ -14,18 +14,29 @@ function createPackagejson (packageJson, configJson, repoFullName, version) {
 	const packageTemplate = JSON.parse(fs.readFileSync(packageJson).toString());
 	packageTemplate.name = config.title.replace(/ /g, '-');
 	// eslint-disable-next-line no-template-curly-in-string
-	packageTemplate.build.artifactName = config.title + '-${version}.${ext}'; // '' is used on purpose, we want the spring to contain ${ext} as text
-	packageTemplate.version = version;
+	packageTemplate.build.artifactName = config.title + '-${version}.${ext}';
+
+	const uniqueIdentifier = process.env.GITHUB_SHA ? process.env.GITHUB_SHA.substring(0, 7) : Date.now().toString().substring(5);
+	packageTemplate.version = `${version}-${uniqueIdentifier}`;
+
+	const [owner, repo] = repoFullName.split('/');
 	packageTemplate.repository = `github:${repoFullName}`;
 	packageTemplate.build.appId = `com.springrts.launcher.${repoDotName}`;
-    packageTemplate.build.publish = undefined;
+	packageTemplate.build.publish = [{
+		provider: "github",
+		owner: owner,
+		repo: repo,
+		releaseType: "release"
+	}];
+
 	if (config.dependencies != null) {
+		packageTemplate.dependencies = packageTemplate.dependencies || {};
 		for (const dependency in config.dependencies) {
 			packageTemplate.dependencies[dependency] = config.dependencies[dependency];
 		}
 	}
 
-	fs.writeFileSync(packageJson, JSON.stringify(packageTemplate), 'utf8');
+	fs.writeFileSync(packageJson, JSON.stringify(packageTemplate, null, 2), 'utf8');
 }
 
 if (require.main === module) {
@@ -35,5 +46,5 @@ if (require.main === module) {
 		process.exit(-1);
 	}
 
-    createPackagejson(args[2], args[3], args[4], args[5])
+	createPackagejson(args[2], args[3], args[4], args[5]);
 }
