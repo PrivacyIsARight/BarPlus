52d34da7da37ddbd1281d6974e5e480dd5836b37
vexalous <vexalous@gmail.com>
2026-09-12 23:24:37 -0400

    Fix menu lag from disable-nonbar widget loop
    Signed-off-by: vexalous <vexalous@gmail.com>

--- LuaMenu/widgets/api_disable_nonbar_widgets.lua
+++ LuaMenu/widgets/api_disable_nonbar_widgets.lua
@@ -14,33 +14,7 @@ end
 
 --------------------------------------------------------------------
 
-local unusedwidgets = {
-	--[[Community Window]]
-	[[Party status panel]],
-	[[bad widgets too]]
-}
-
 function widget:Initialize()
-end
-
-local removedwidgets = {}
-
-function widget:Update()
-	if widgetHandler:FindByName("Image Preloader") then
-		widgetHandler:DisableWidget("Image Preloader")
-	end
 	widgetHandler:EnableWidget("Rapid Pool Cache")
-	for i, widgetname in ipairs(unusedwidgets) do 
-		
-		local wijjit = widgetHandler:FindByName(widgetname)
-		if wijjit then
-			--Spring.Echo("Disable NonBar Widgets:", widgetname)
-			removedwidgets[#removedwidgets+1] = widgetname
-			widgetHandler:RemoveWidget(wijjit)
-		end
-	end
-	Spring.Echo("Disabled NonBar Widgets:", table.concat(removedwidgets, ","))
-	
-	widgetHandler:RemoveWidget(widgetHandler:FindByName("Disable NonBar Widgets"))
 end
 
