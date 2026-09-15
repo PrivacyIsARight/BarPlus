f98856e25f661689f029e119d4128b2964ff9443
vexalous <vexalous@gmail.com>
2026-09-15 15:09:22 -0400

    Update BarPlus version string format
    Signed-off-by: vexalous <vexalous@gmail.com>

--- libs/liblobby/lobby/interface.lua
+++ libs/liblobby/lobby/interface.lua
@@ -42,7 +42,7 @@ local function GetLobbyName()
 			break
 		end
 	end
-	local lobbyname = 'BarPlus:'..byarchobbyrapidTag
+	local lobbyname = 'BarPlus Version '..byarchobbyrapidTag
 	--Spring.Utilities.TraceFullEcho()
 	return lobbyname
 end
